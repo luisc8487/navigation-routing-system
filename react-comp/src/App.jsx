98fd3d9ab245177e89b68df7cdee07df5db00f32
L C <LF9904@gmail.com>
2025-05-26 20:14:10 -0400

    used React Icons

--- react-comp/src/App.jsx
+++ react-comp/src/App.jsx
@@ -1,21 +1,26 @@
+import {GoBell, GoDownload, GoDatabase} from "react-icons/go";
 import Button from "./Button";
-import "./index.css";
 
 function App() {
   return (
     <div>
       <div>
         <Button success rounded outline>
+          <GoBell />
           Click me!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoDownload />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
