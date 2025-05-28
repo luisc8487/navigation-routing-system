40d482fa9d1ec18451ee13ab4e05355e1450bc2b
L C <LF9904@gmail.com>
2025-05-27 23:49:38 -0400

    component setup

--- react-comp/src/App.jsx
+++ react-comp/src/App.jsx
@@ -1,43 +1,24 @@
-import {GoBell, GoDownload, GoDatabase} from "react-icons/go";
-import Button from "./components/Button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log("Hi");
-  };
-
-  return (
-    <div>
-      <div>
-        <Button success rounded outline className="mb-5" onClick={handleClick}>
-          <GoBell />
-          Click me!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <GoDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      label: "Can I use React on a Project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      label: "Can I use JavaScript on a Project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      label: "Can I use CSS on a Project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
