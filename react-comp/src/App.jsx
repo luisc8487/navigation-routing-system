5efe5f0ee2c2a56c3263d4f39064de09587f1c44
L C <LF9904@gmail.com>
2025-05-28 16:45:55 -0400

    mapping out button component

--- react-comp/src/App.jsx
+++ react-comp/src/App.jsx
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "asdfk",
       label: "Can I use React on a Project?",
       content:
         "You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "dskfn",
       label: "Can I use JavaScript on a Project?",
       content:
         "You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "enfonr",
       label: "Can I use CSS on a Project?",
       content:
         "You can use React on any project you want. You can use React on any project you want.",
