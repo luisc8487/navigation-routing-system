a99665c9ba841137369392d6ab360ad0225fe4ec
L C <LF9904@gmail.com>
2025-06-09 17:06:07 -0400

    added a reuseable panel

--- react-comp/src/components/Dropdown.jsx
+++ react-comp/src/components/Dropdown.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {IoIosArrowDown} from "react-icons/io";
+import Panel from "./Panel";
 
 function Dropdown({options, value, onChange}) {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,18 +30,14 @@ function Dropdown({options, value, onChange}) {
 
   return (
     <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
       >
         {value ? value.label : "Select..."}
-        <IoIosArrowDown className="text-lg"/>
-      </div>
-      {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
-          {renderedOptions}
-        </div>
-      )}
+        <IoIosArrowDown className="text-lg" />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 }
