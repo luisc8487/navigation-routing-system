ca257b37dfba38b2a3edf33d1d0e13a9aeb10c29
L C <LF9904@gmail.com>
2025-06-18 22:55:44 -0400

    added a modal component

--- react-comp/src/components/Sidebar.jsx
+++ react-comp/src/components/Sidebar.jsx
@@ -5,12 +5,17 @@ function Sidebar() {
     {label: "Dropdown", path: "/"},
     {label: "Accordion", path: "/accordion"},
     {label: "Buttons", path: "/buttons"},
+    {label: "Modal", path: "/modal"},
   ];
 
   const renderedLinks = links.map((link) => {
     return (
-      <Link key={link.label} to={link.path} className="mb-3"
-      activeClassName="font-bold border-l-4 border-blue-500 pl-2">
+      <Link
+        key={link.label}
+        to={link.path}
+        className="mb-3"
+        activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+      >
         {link.label}
       </Link>
     );
