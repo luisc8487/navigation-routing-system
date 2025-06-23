ef57f02abfbecca3d571e237b17ed8068e3e8fec
L C <LF9904@gmail.com>
2025-06-23 16:13:04 -0400

    added SortableTable

--- react-comp/src/pages/TablePage.jsx
+++ react-comp/src/pages/TablePage.jsx
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -12,12 +13,18 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
     },
-    {label: "Score", render: (fruit) => fruit.score, header: () => <th className="bg-red-500">Score</th>},
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+      //   header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
 
   const keyFn = (fruit) => {
@@ -26,7 +33,7 @@ function TablePage() {
 
   return (
     <>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </>
   );
 }
