1cb6a5c09f51d9ff87ad8b5ac851e258d06a794b
L C <LF9904@gmail.com>
2025-07-01 23:31:27 -0400

    added the form

--- react-comp/src/pages/CounterPage.jsx
+++ react-comp/src/pages/CounterPage.jsx
@@ -1,13 +1,32 @@
-import useCounter from "../hooks/use-counter";
+import {useState} from "react";
 import Button from "../components/Button";
+import Panel from "../components/Panel";
 
 function CounterPage({initalCount}) {
-  const {count, increment} = useCounter(initalCount);
+  const {count, setCount} = useState(initalCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div>
+        <Button onClick={decrement}>Increment</Button>
+        <Button onClick={increment}>Increment</Button>
+      </div>
+
+      <form>
+        <label>Add a lot!</label>
+        <input type="number" className="p-1 m-3 bg-gray-50 border border-gray-300"/>
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
 export default CounterPage;
