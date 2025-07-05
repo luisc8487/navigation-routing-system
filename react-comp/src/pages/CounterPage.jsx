125e95b7ff4dbd6cf9b74288dcf9704e0b35362d
L C <LF9904@gmail.com>
2025-07-05 00:09:15 -0400

    useReducer in Action

--- react-comp/src/pages/CounterPage.jsx
+++ react-comp/src/pages/CounterPage.jsx
@@ -1,23 +1,32 @@
-import {useState} from "react";
+import {useReducer} from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const reducer = (state, action) => {
+  //
+};
+
 function CounterPage({initalCount}) {
-  const [count, setCount] = useState(initalCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initalCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: initalCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
 
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
 
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   // Add Secction here
@@ -25,13 +34,13 @@ function CounterPage({initalCount}) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div>
         <Button onClick={decrement}>Increment</Button>
         <Button onClick={increment}>Increment</Button>
@@ -40,7 +49,7 @@ function CounterPage({initalCount}) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
