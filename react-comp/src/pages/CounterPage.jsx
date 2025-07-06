2897a74859b9b82704ca2ce4af68a22901768716
L C <LF9904@gmail.com>
2025-07-06 16:24:40 -0400

    added const action types

--- react-comp/src/pages/CounterPage.jsx
+++ react-comp/src/pages/CounterPage.jsx
@@ -2,22 +2,26 @@ import {useReducer} from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'set_value_to_add';
+
 const reducer = (state, action) => {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT_COUNT) {
     return {
       ...state,
       count: state.count + 1,
     };
   }
 
-  if (action.type === "decrement") {
+  if (action.type === DECREMENT_COUNT) {
     return {
       ...state,
       count: state.count - 1,
     };
   }
 
-  if (action.type === "change-value-to-add") {
+  if (action.type === SET_VALUE_TO_ADD) {
     return {
       ...state,
       valueToAdd: action.payload,
@@ -52,7 +56,7 @@ function CounterPage({initalCount}) {
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
 
-    dispatch({type: "change-value-to-add", payload: value});
+    dispatch({type: SET_VALUE_TO_ADD, payload: value});
     // setToAddValue(value)
   };
 
